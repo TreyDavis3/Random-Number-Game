ea55cc103b4862df750b2708271e16ead613c43c
Trey Davis <89280903+TreyDavis3@users.noreply.github.com>
2025-05-20 16:46:11 -0600

    Added a play again option

--- .vscode/Number_Generator.c
+++ .vscode/Number_Generator.c
@@ -4,29 +4,41 @@
 
 int main() {
     // Seed the random number generator
-    srand(time(NULL));
-    int secretNumber = rand() % 100 + 1; // Generates a number between 1 and 100
-    int guess;
-    int attempts = 0;
-    printf("Welcome to the Guessing Game!\n");
-    printf("I'm thinking of a number between 1 and 100. Try to guess it.\n");
+    char playAgain; // Declare playAgain
 
-    do {
-        printf("Enter your guess: ");
-        while (scanf("%d", &guess) != 1 || guess < 1 || guess > 100) {
-            printf("Invalid input. Please enter a number between 1 and 100: ");
-            while (getchar() != '\n'); // Clear the invalid input
+    do{
+        srand(time(NULL));
+        int secretNumber = rand() % 100 + 1; // Generates a number between 1 and 100
+        int guess;
+        int attempts = 0;
+
+        printf("Welcome to the Guessing Game!\n");
+        printf("I'm thinking of a number between 1 and 100. Try to guess it.\n");
+
+        do {
+            printf("Enter your guess: ");
+            while (scanf("%d", &guess) != 1 || guess < 1 || guess > 100) {
+                printf("Invalid input. Please enter a number between 1 and 100: ");
+                while (getchar() != '\n'); // Clear the invalid input
+            }
+            attempts++;
+
+            if (guess > secretNumber) {
+                printf("Too high! Try again.\n");
+            } else if (guess < secretNumber) {
+                printf("Too low! Try again.\n");
+            } else {
+                printf("Congratulations! You guessed the number %d in %d attempts!\n", secretNumber, attempts);
+            }
+        } while (guess != secretNumber);
+
+        // Ask the user if they want to play again
+        printf("Do you want to play again? (y/n): ");
+        scanf(" %c", &playAgain);
+        if (playAgain != 'y' && playAgain != 'Y') {
+            printf("Thanks for playing! Goodbye!\n");
+            break;
         }
-        attempts++;
-
-        if (guess > secretNumber) {
-            printf("Too high! Try again.\n");
-        } else if (guess < secretNumber) {
-            printf("Too low! Try again.\n");
-        } else {
-            printf("Congratulations! You guessed the number %d in %d attempts!\n", secretNumber, attempts);
-        }
-    } while (guess != secretNumber);
-
+    }while(playAgain == 'y' || playAgain == 'Y');
     return 0;
 }
